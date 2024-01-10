08a39de2313f437f3cae1bddae50056f16156d51
Gassama <gassma803@gmail.com>
2024-01-10 01:51:08 +0100

    toujours sur le javaScrip,apparition du corps de notre page avec transition ok, et aussi apparition de la barre de navigation après qu'il soit hidden

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ themeToggleButton.addEventListener('click', () => {
     const isLightTheme = body.classList.contains('light-theme');
     themeToggleButton.innerHTML = isLightTheme ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
 });
+
+//effet de transition sur les element de notre page, à part la navbar
+document.addEventListener("DOMContentLoaded",function () {
+    setTimeout(function(){
+        var transitionDiv = document.querySelector(".transition");
+        var navbar = document.querySelector(".navbar");
+
+        //Reduire la hauteur de la div .transition à 0
+        transitionDiv.style.height="100px";
+
+    },1000)
+  })
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.navbar').style.visibility = 'visible';
+        document.querySelector('.navbar').style.opacity = 1;
+    }, 1800); // Délai de 1000 millisecondes (1 seconde)
+});
+
+
+
+
